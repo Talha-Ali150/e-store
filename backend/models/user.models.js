dad3e5b6a69d45554944cc9f965ac42047c62cb1
Talha-Ali150 <syed.talha.ali.150@gmail.com>
2024-03-18 17:01:02 +0500

    changes in user model for tokens

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -20,7 +20,9 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
-  tokens: [String],
+  refreshToken: {
+    type: String,
+  },
 });
 
 userSchema.pre("save", async function (next) {
@@ -36,12 +38,31 @@ userSchema.methods.comparePassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-userSchema.methods.generateToken = async function () {
-  const _id = this._id;
-  const token = jwt.sign({ _id }, process.env.SERVER_SECRET);
-  this.tokens.push(token);
-  await this.save();
-  return token;
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
 };
 
 const User = mongoose.model("User", userSchema);
